6da9e3b039321349b8043035106c84ebb731ff20
Piekala <lpsevero@inf.ufsm.br>
2024-11-21 15:11:48 -0300

    logo changes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Login from './src/pages/not-auth/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Login />
+      
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ assets/styles/pages/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const Screen = StyleSheet.create({
+
+    light: {
+
+        height: '100%',
+        width: '100%',
+        
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+
+});
+
+export const Card = StyleSheet.create({
+
+
+
+});
